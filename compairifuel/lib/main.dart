62813f33e5fa2d129233c0501cf32d033132449e
Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
2023-12-01 16:40:03 +0100

    added map page en class om geolocator te gebruiken om long en lat op te halen van de device.

--- compairifuel/lib/main.dart
+++ compairifuel/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:compairifuel/pages/map.dart';
 
 void main() {
   runApp(const MyApp());
@@ -44,7 +45,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MapPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
