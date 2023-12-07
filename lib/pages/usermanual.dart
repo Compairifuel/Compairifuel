9b1a01011d70c59109d5cdd46918fb9a77902b77
Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
2023-12-07 19:05:38 +0100

    pagination usermanual (#67)
    * pagination functionaliteit werkt
    
    * usermanual transformed pagination to a more comfortable pagination.
    
    ---------
    
    Signed-off-by: Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
    Co-authored-by: Alper Bekar <117603269+altergith@users.noreply.github.com>

--- lib/pages/usermanual.dart
+++ lib/pages/usermanual.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/navbar.dart';
-// import 'package:flutter/services.dart' show rootBundle;
-
-class UserManualPage extends StatelessWidget {
+class UserManualPage extends StatefulWidget {
   const UserManualPage({super.key, required this.title});
 
   final String title;
 
   @override
+  State<UserManualPage> createState() => _UserManualPageState();
+}
+
+class _UserManualPageState extends State<UserManualPage> {
+  int _currentIndex = 0;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    debugPrint("UserManualPage didChangeDependencies: $_currentIndex");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final PageController controller =
+        PageController(initialPage: _currentIndex);
     return Scaffold(
       body: Column(
         children: <Widget>[
@@ -42,36 +54,120 @@ class UserManualPage extends StatelessWidget {
               )
             ],
           ),
-          GestureDetector(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2,
-              child: const Text('This is the usermanual about compairifuel. we will explain how to use this app. please read this manual carefully. if you have any question, please contact us.',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontFamilyFallback: <String>['RobotoSans'],
-                  fontSize: 18,
-                ),
-              ),
-            ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2,
+            child: PageView(
+                controller: controller,
+                scrollDirection: Axis.horizontal,
+                children: const <Widget>[
+                  Text(
+                    'This is the usermanual about compairifuel.'
+                    ' we will explain how to use this app. please read this manual carefully.'
+                    ' if you have any question, please contact us.',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontFamilyFallback: <String>['RobotoSans'],
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'second page of usermanual',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontFamilyFallback: <String>['RobotoSans'],
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+                onPageChanged: (int index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                }),
           ),
           Column(
             children: <Widget>[
               Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      color: const Color(0xFFFF0000),
-                      height: 8,
-                      child: const Text(''),
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 8,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _currentIndex = 0;
+                            });
+                            controller.animateToPage(_currentIndex,
+                                duration: const Duration(milliseconds: 500),
+                                curve: Curves.easeInOut);
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: () {
+                              if (_currentIndex == 0) {
+                                return MaterialStateProperty.all<Color>(
+                                    Colors.grey);
+                              } else {
+                                return MaterialStateProperty.all<Color>(
+                                    Colors.white);
+                              }
+                            }(),
+                            shape: MaterialStateProperty.all<CircleBorder>(
+                              const CircleBorder(
+                                side: BorderSide(
+                                  color: Colors.grey,
+                                  width: 1,
+                                  style: BorderStyle.solid,
+                                ),
+                              ),
+                            ),
+                          ),
+                          child: const Text(''),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _currentIndex = 1;
+                            });
+                            controller.animateToPage(_currentIndex,
+                                duration: const Duration(milliseconds: 500),
+                                curve: Curves.easeInOut);
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: () {
+                              if (_currentIndex == 1) {
+                                return MaterialStateProperty.all<Color>(
+                                    Colors.grey);
+                              } else {
+                                return MaterialStateProperty.all<Color>(
+                                    Colors.white);
+                              }
+                            }(),
+                            shape: MaterialStateProperty.all<CircleBorder>(
+                              const CircleBorder(
+                                side: BorderSide(
+                                  color: Colors.grey,
+                                  width: 1,
+                                  style: BorderStyle.solid,
+                                ),
+                              ),
+                            ),
+                          ),
+                          child: const Text(''),
+                        ),
+                      ],
                     ),
-                  ]),
+                  ),
+                ],
+              ),
             ],
           )
         ],
       ),
-      //bottomNavigationBar: const NavBar(),
     );
   }
 }
