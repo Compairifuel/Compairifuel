a718bf02dd9291ed0abb8914984a6a83e9cd9403
Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
2023-12-10 13:17:30 +0100

    map marker werkt, bezig met camera en maptiles

--- compairifuel/lib/pages/map.dart
+++ compairifuel/lib/pages/map.dart
@@ -41,7 +41,9 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   final LocationService _locationService = LocationService();
-  late LatLng _userLocation;
+  late LatLng? _userLocation;
+  final MapController _mapController = MapController();
+  // late CameraFit _cameraFit = CameraConstraint.containCenter(bounds: LatLngBounds(_userLocation!,_userLocation!);
 
   @override
   void initState() {
@@ -49,6 +51,17 @@ class _MapPageState extends State<MapPage> {
     _locationService.startListeningLocationUpdates().listen((Position position) {
       setState(() {
         _userLocation = LatLng(position.latitude, position.longitude);
+
+        if(_userLocation != null){
+          final cameraFit = CameraFit.bounds(
+            bounds: LatLngBounds(_userLocation!,_userLocation!),
+            padding: const EdgeInsets.all(8.0),
+          );
+
+          cameraFit.fit(_mapController.camera);
+          _mapController.fitCamera(cameraFit);
+          _mapController.move(_userLocation!, 0.0);
+        }
       });
     });
   }
@@ -58,25 +71,57 @@ class _MapPageState extends State<MapPage> {
     return Scaffold(
       body: Center(
         child: FlutterMap(
+          mapController: _mapController,
           options: MapOptions(
-            center: _userLocation ?? LatLng(0, 0),
-            zoom: 13.0,
+            initialCenter: _userLocation!,
+            // initialCameraFit: CameraFit.bounds(
+            //   bounds: _mapController.camera.visibleBounds,
+            // ),
+            // initialZoom: 0.0,
+            // onMapReady: () {
+            //   _mapController.move(
+            //   _userLocation!,
+            //       0.0,
+            //   );
+            // },
+            // interactionOptions: InteractionOptions(
+            //   flags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+            // ),
+            // initialCameraFit: CameraFit,
+            // initialCenter: _userLocation ?? const LatLng(0, 0),
+
+            // cameraConstraint: CameraConstraint.containCenter(bounds: LatLngBounds(_userLocation!,_userLocation!),
+            // ),
+
+
+            // CameraFit.bounds(
+            //   bounds: LatLngBounds(_userLocation!,_userLocation!),
+            //   padding: const EdgeInsets.all(8.0),
+            // ),
+
           ),
-          layers: [
-            TileLayer(
-              urlTemplate:
-              'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-              subdomains: ['a', 'b', 'c'],
-              //useragend: ik heb geen idee wat dit is
-            ),
+          children: [
+              TileLayer(
+                urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                //https://{baseURL}/map/{versionNumber}/tile/{layer}/{style}/{zoom}/{X}/{Y}.{format}?key={Your_API_Key}
+                // tileBounds: LatLngBounds(_userLocation!,_userLocation!),
+                //useragend: hebben we m nog nodig?
+              ),
             MarkerLayer(
               markers: [
-                if (_userLocation != null)
                   Marker(
                     width: 40.0,
                     height: 40.0,
-                    point: _userLocation,
-                      child: FlutterLogo(),
+                    point: _userLocation!,
+                    child: GestureDetector(
+                      onTap: () {
+                        debugPrint("Marker tapped");
+                      },
+                      child: const ImageIcon(
+                        AssetImage("assets/images/location.png"),
+                        size: 24,
+                      ),
+                    ),
                   ),
               ],
             ),
