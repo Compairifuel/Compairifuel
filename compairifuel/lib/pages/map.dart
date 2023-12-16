056335c0c67214f936903c449b7d0f638c62c37b
Jelle Schaafsma <55688800+Mino03947@users.noreply.github.com>
2023-12-16 11:04:57 +0100

    map (#76)
    * soort van icons
    
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>
    
    * soort van icons
    
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>
    
    * kapotttt
    
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>
    
    * map marker werkt, bezig met camera en maptiles
    
    * Changes by Alper
    
    * locatie werkt weer
    
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>
    
    * locatie werkt weer
    
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>
    
    * print weggehaald
    
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>
    
    ---------
    
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>
    Co-authored-by: Alper Bekar <a.bekar@student.han.nl>

--- compairifuel/lib/pages/map.dart
+++ compairifuel/lib/pages/map.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 
 void startListeningLocationUpdates() {
   Geolocator.getPositionStream().listen((Position position) {
-    print(position);
-
-    // Make request to TomTom Search API
-    print('TomTom API request sent');
   });
 }
 
@@ -52,17 +50,14 @@ class LocationService {
         desiredAccuracy: LocationAccuracy.high);
   }
 
-  void startListeningLocationUpdates() {
-    Geolocator.getPositionStream().listen((Position position) {
-      searchNearby(position.latitude, position.longitude);
-      print(position);
-    });
+  Stream<Position> startListeningLocationUpdates() {
+    return Geolocator.getPositionStream();
   }
 
 }
 
 class MapPage extends StatefulWidget {
-  const MapPage({super.key, required this.title});
+  const MapPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -72,33 +67,103 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   final LocationService _locationService = LocationService();
+  Position? _currentPosition;
+  late LatLng? _userLocation = LatLng(0, 0);
+  final MapController _mapController = MapController();
+  // late CameraFit _cameraFit = CameraConstraint.containCenter(bounds: LatLngBounds(_userLocation!,_userLocation!);
 
   @override
   void initState() {
     super.initState();
-    _locationService.startListeningLocationUpdates();
+    debugPrint(_locationService.startListeningLocationUpdates().toString());
+    if(_locationService.startListeningLocationUpdates().toString() == "Instance of 'Stream<Position>'") {
+      _locationService.startListeningLocationUpdates();
+    } else {
+      _locationService.getCurrentLocation().then((Position position) {
+        setState(() {
+          _userLocation = LatLng(position.latitude, position.longitude);
+        });
+      });
+    }
+    _locationService.startListeningLocationUpdates().listen((Position position) {
+      setState(() {
+        _userLocation = LatLng(244, 9999);
+        // _userLocation = LatLng(position.latitude, position.longitude);
+
+        if(_userLocation != null) {
+          final cameraFit = CameraFit.bounds(
+            bounds: LatLngBounds(_userLocation!,_userLocation!),
+            padding: const EdgeInsets.all(8.0),
+          );
+
+          cameraFit.fit(_mapController.camera);
+          _mapController.fitCamera(cameraFit);
+          _mapController.move(_userLocation!, 0.0);
+        }
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: StreamBuilder<Position>(
-        stream: _locationService.getCurrentLocation().asStream(),
-        builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              Position position = snapshot.data!;
-              return Text(
-                'Latitude: ${position.latitude}, Longitude: ${position.longitude}',
-              );
-            }
-            else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-              }
-            else {
-              return CircularProgressIndicator();
-            }
-          },
+        child: FlutterMap(
+          mapController: _mapController,
+          options: MapOptions(
+            initialCenter: _userLocation!,
+            // initialCameraFit: CameraFit.bounds(
+            //   bounds: _mapController.camera.visibleBounds,
+            // ),
+            // initialZoom: 0.0,
+            // onMapReady: () {
+            //   _mapController.move(
+            //   _userLocation!,
+            //       0.0,
+            //   );
+            // },
+            // interactionOptions: InteractionOptions(
+            //   flags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+            // ),
+            // initialCameraFit: CameraFit,
+            // initialCenter: _userLocation ?? const LatLng(0, 0),
+
+            // cameraConstraint: CameraConstraint.containCenter(bounds: LatLngBounds(_userLocation!,_userLocation!),
+            // ),
+
+
+            // CameraFit.bounds(
+            //   bounds: LatLngBounds(_userLocation!,_userLocation!),
+            //   padding: const EdgeInsets.all(8.0),
+            // ),
+
+          ),
+          children: [
+              TileLayer(
+                urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                //https://{baseURL}/map/{versionNumber}/tile/{layer}/{style}/{zoom}/{X}/{Y}.{format}?key={Your_API_Key}
+                // tileBounds: LatLngBounds(_userLocation!,_userLocation!),
+                //useragend: hebben we m nog nodig?
+              ),
+            MarkerLayer(
+              markers: [
+                  Marker(
+                    width: 40.0,
+                    height: 40.0,
+                    point: _userLocation!,
+                    child: GestureDetector(
+                      onTap: () {
+                        debugPrint("Marker tapped");
+                      },
+                      child: const ImageIcon(
+                        AssetImage("assets/images/location.png"),
+                        size: 24,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ],
         ),
       ),
     );
