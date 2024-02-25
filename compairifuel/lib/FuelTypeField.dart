18b3a32acf2294ffe6ff6b494c4e5cde0c303437
Alper Bekar <a.bekar@student.han.nl>
2024-02-25 15:47:12 +0100

    geen idee Alper commit zomaar iets z toestmmin ge
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>

---
+++ compairifuel/lib/FuelTypeField.dart
@@ -0,0 +1,22 @@
+class FuelTypeField extends StatefulWidget implements UIComponent {
+  List<String> optionList;
+
+  State<FuelTypeField> createState() {
+    _FuelTypeFieldState();
+  }
+
+  FuelTypeField({super.key});
+
+  build(BuildContext context){
+    //TODO
+  }
+
+}
+
+class _FuelTypeFieldState extends State<FuelTypeField> {
+  FuelOption currentOption;
+
+  build(BuildContext context){
+    //TODO
+  }
+}
---
+++ compairifuel/lib/MapMarker.dart
@@ -0,0 +1,7 @@
+class MapMarker extends Marker {
+  String image;
+
+  MapMarker(String image){
+    this.image = image;
+  }
+}
