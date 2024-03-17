2b3079179418bd33dc52f8ad475efd3a269c1427
Alper Bekar <a.bekar@student.han.nl>
2024-03-17 11:33:30 +0100

    geen idee Alper commit zomaar iets met toestemming
    Signed-off-by: Alper Bekar <a.bekar@student.han.nl>

--- compairifuel/test/widget_test.dart
+++ compairifuel/test/widget_test.dart
@@ -5,24 +5,33 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:compairifuel/NavIcon.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:compairifuel/Navbar.dart';
+import 'package:compairifuel/Pagination.dart';
+import 'package:compairifuel/Page.dart' as PageComponent;
+import 'package:compairifuel/UserManual.dart';
+import 'package:compairifuel/Main.dart';
 
 
 void main() {
-  testWidgets(skip:true,'Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets(skip: false, 'navIcon test', (WidgetTester tester) async {
+    await tester.pumpWidget(NavIcon(AssetImage("assets/images/map.png")));
+
+    final navIcon = find.byType(NavIcon);
+    //https://api.flutter.dev/flutter/flutter_test/CommonFinders/byType.html
+    expect(navIcon, findsOneWidget);
+  });
+
+  testWidgets(skip: true, 'myapp test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    // await tester.pumpWidget(MyApp(pages: [PageComponent.Page(title: "title", content: UserManual(), navbar: const Navbar(navItemList: []), pagination: Pagination())],));
-    //
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-    //
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-    //
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    // await tester.pumpWidget(MyApp(key:, pages: [
+    //   PageComponent.Page(title: "title",
+    //       content: UserManual(),
+    //       navbar: Navbar(navItemList: []),
+    //       pagination: Pagination())
+    // ],
+    // ));
   });
 }
