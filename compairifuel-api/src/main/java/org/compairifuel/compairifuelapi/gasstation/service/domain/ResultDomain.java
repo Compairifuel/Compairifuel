fadf6d4ae30a538f1e95057f63fe77d49166539a
Jelle Schaafsma <JJ.Schaafsma1@student.han.nl>
2024-07-15 13:44:05 +0200

    Domain classes moved to domain package, Added ServiceAggregatorAdapter, Added TomTomAdaptee, Renamed ServiceUtils to ServiceHttpClient, Refactored ServiceHttpClient to use CDI, Refactored FuelPriceService to use ServiceHttpClient, Refactored GasStationService to use AggregatorAdapter, Refactored static logger attributes to @Log annotation. [VERIFIED]: Tests passed (1/1).

--- compairifuel-api/src/main/java/org/compairifuel/compairifuelapi/gasstation/service/domain/ResultDomain.java
+++ compairifuel-api/src/main/java/org/compairifuel/compairifuelapi/gasstation/service/domain/ResultDomain.java
@@ -1,7 +1,6 @@
-package org.compairifuel.compairifuelapi.gasstation.service;
+package org.compairifuel.compairifuelapi.gasstation.service.domain;
 
 import lombok.Data;
-import lombok.Getter;
 
 import java.util.List;
 
--- compairifuel-api/src/main/java/org/compairifuel/compairifuelapi/utils/service/ServiceHttpClientImpl.java
+++ compairifuel-api/src/main/java/org/compairifuel/compairifuelapi/utils/service/ServiceHttpClientImpl.java
@@ -8,12 +8,22 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.Response;
 import lombok.Cleanup;
+import lombok.extern.java.Log;
 
-import java.util.logging.Logger;
+@Log(topic = "ServiceHttpClientImpl")
+@Default
+public class ServiceHttpClientImpl implements IServiceHttpClient {
 
-public class ServiceUtils {
-    private final Logger logger = Logger.getLogger(this.getClass().getName());
-
+    /**
+    * Sends a request to the given url with the given query parameters.
+    *
+    * @param url: The url to send the request to.
+    * @param queryParams: The query parameters to send with the request.
+    * @param cls: The class to map the response to.
+    * @return T: The response mapped to the class.
+    * @throws RuntimeException: When the response status is not 200.
+     **/
+    @Override
     public <T> T sendRequest(String url, MultivaluedHashMap<String, Object> queryParams, Class<T> cls) {
         @Cleanup Client client = ClientBuilder.newClient();
         WebTarget target = client.target(url);
@@ -28,7 +38,7 @@ public class ServiceUtils {
 
         @Cleanup Response response = target.request(MediaType.APPLICATION_JSON).headers(headers).get(Response.class);
         if(response.getStatus() != 200) {
-            logger.severe("Failed to get gas stations from TomTom API. Status code: " + response.getStatus());
+            log.severe("Failed to get gas stations from TomTom API. Status code: " + response.getStatus());
             throw new RuntimeException("Failed to get gas stations from TomTom API. Status code: " + response.getStatus());
         }
 
