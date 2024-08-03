897e090c12c7237042f976ce4d39dcb075a8cb55
Jelle Schaafsma <JJ.Schaafsma1@student.han.nl>
2024-08-03 17:12:26 +0200

    ExceptionMappers added for Constraints and NotFound, data.csv updated with fuelType, Added Exception catchers to DataCSVFuelPriceServiceAdaptee, removed try catch in FuelPriceService, added alternative GET requests in FuelPriceController, added Unittests for GasStationController, FuelPriceController, GasStationService, FuelPriceService, DataCSVFuelPriceService. [VERIFIED]: Tests passed (18/18).

--- compairifuel-api/src/main/java/org/compairifuel/compairifuelapi/fuelprice/presentation/FuelPriceController.java
+++ compairifuel-api/src/main/java/org/compairifuel/compairifuelapi/fuelprice/presentation/FuelPriceController.java
@@ -1,6 +1,8 @@
 package org.compairifuel.compairifuelapi.fuelprice.presentation;
 
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -22,6 +24,22 @@ public class FuelPriceController {
     @Path("/prices/{fuelType}/{address}")
     public Response getPrices(@PathParam("fuelType") String fuelType, @PathParam("address") String address) {
         List<FuelPriceResponseDTO> prices = fuelPriceService.getPrices(fuelType, address);
+        return Response.ok().entity(prices).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/prices/{fuelType}/{latitude}/{longitude}")
+    public Response getPrices(@PathParam("fuelType") String fuelType, @PathParam("latitude") @Min(value=-180,message="latitude cannot be less than -180.") @Max(value=180,message="latitude cannot be greater that 180.") double latitude, @PathParam("longitude") @Min(value=-180,message="longitude cannot be less than -180.") @Max(value=180,message="longitude cannot be greater that 180.") double longitude) {
+        List<FuelPriceResponseDTO> prices = fuelPriceService.getPrices(fuelType, latitude, longitude);
+        return Response.ok().entity(prices).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/prices/{fuelType}/{address}/{latitude}/{longitude}")
+    public Response getPrices(@PathParam("fuelType") String fuelType,@PathParam("address") String address, @PathParam("latitude") @Min(value=-180,message="latitude cannot be less than -180.") @Max(value=180,message="latitude cannot be greater that 180.") double latitude, @PathParam("longitude") @Min(value=-180,message="longitude cannot be less than -180.") @Max(value=180,message="longitude cannot be greater that 180.") double longitude) {
+        List<FuelPriceResponseDTO> prices = fuelPriceService.getPrices(fuelType, address, latitude, longitude);
         return Response.ok().entity(prices).build();
     }
 }
