62c037c88495c5f050fff0a3bb54aed2011841cb
Jelle Schaafsma <JJ.Schaafsma1@student.han.nl>
2024-12-30 15:28:48 +0100

    feat(auth): integrate refactored oauth in frontend
    Signed-off-by: Jaron Zijlstra <jaron_zijlstra@outlook.com>

--- compairifuel/lib/main.dart
+++ compairifuel/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:compairifuel/src/app.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const ProviderScope(child: MyApp()));
 }
