dd2f572aecf2b77297a7d29cdc57a9bf5d212b15
Jaron Zijlstra <support@jaronline.dev>
2025-10-06 12:44:01 +0200

    fix: localizations package not found

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:compairifuel/src/global/theme/theme.dart';
 import 'package:compairifuel/src/routers.dart';
-import "package:flutter_gen/gen_l10n/app_localizations.dart";
+import "package:compairifuel/localization/app_localizations.dart";
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
