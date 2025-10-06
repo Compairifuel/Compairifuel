cfae013340723a8425afe28952e87b800d6e3002
Jaron Zijlstra <support@jaronline.dev>
2025-10-06 12:44:01 +0200

    fix: localizations package not found

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import "package:flutter_gen/gen_l10n/app_localizations.dart";
+import "package:compairifuel/localization/app_localizations.dart";
 
 class EnvWidget extends StatelessWidget {
   final Widget child;
